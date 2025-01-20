2e4bfebeeccf4bac5114a6e2f5f978d49f2f9362
Bruno L Nogueira <b.nogueira@protonmail.com>
2025-01-19 22:31:26 -0300

    ......

--- src/views/Room/index.tsx
+++ src/views/Room/index.tsx
@@ -104,7 +104,7 @@ const Room = (props: {host: boolean}) => {
                                 <S.CardContainer posright={index*-0.25} key={`board__top__card-${index}`} >
                                 <Card
                                     attributes={card}
-                                    posbottom={index*205.8}
+                                    posbottom={index*225.8}
                                     faceDown
                                     
                                     onClick={() => {
